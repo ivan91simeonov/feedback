e2b7dafa494d677c0dff09e270655d121563b721
ivan91simeonov <vanches91@abv.bg>
2022-03-31 19:30:40 +0300

    feedback stats component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -17,6 +17,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
